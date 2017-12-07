8697f6f3554fdf2094f394f5c3f2e0544497aaa0
Alex <aorella1@binghamton.edu>
2017-12-07 16:49:43 -0500

    Finished sort function

--- Schedule.cpp
+++ Schedule.cpp
@@ -1,6 +1,33 @@
 #include "Schedule.h"
 #include <iostream>
+#include <string>
+
 using namespace std;
+
 void Schedule::sort_semesters(){
-  std::cout << "sorting >;]" << std::endl;  
+  vector<pair<string, vector<string> > > sorted;
+  while (semesters.size() >0) {
+    //pair<string, vector<string> min = sememsters[0];
+    int min = 0;
+    for (unsigned int i = 0; i < semesters.size(); i++){
+       char sem1 = semesters[i].first[0]; 
+       char sem2 = semesters[min].first[0]; 
+       int year_1 = stoi(semesters[i].first.substr(1, 4));
+       int year_2 = stoi(semesters[min].first.substr(1,4)); 
+
+       if ( year_1 == year_2 && sem1 > sem2){
+          min = i;
+          //sorted.push_back(semesters[i]);
+          //semesters.erase(semesters.begin()+i)
+       } else if (year_1 < year_2){
+          min = i;
+       }
+    }
+    sorted.push_back(semesters[min]);
+    semesters.erase(semesters.begin()+min);
+  }
+  semesters = sorted;
+  for (unsigned int i = 0; i < semesters.size(); i++){
+    cout << "sorted: " << sorted[i].first << endl;
+  }
 }
