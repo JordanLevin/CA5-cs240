d4d29aa1b3e9ebfc469dd062776f2b6223dc44be
sacosta8 <sacosta8@binghamton.edu>
2017-12-06 19:22:46 -0500

    Adding a check for courses being applied that hopefully worksX

--- Requirements.cpp
+++ Requirements.cpp
@@ -1,7 +1,26 @@
 #include "Requirements.h"
+#include <string>
+#include <vector>
 
+using namespace std;
 
 bool Requirements::verify(CourseOffering offerings, 
         std::vector<std::vector<std::string>> schedule){
+        bool good_schedule = true;
+        //Checking if each course is offered
+        for(int i = 0; i < schedule.size(); i++){
+            string name = schedule[i][0];
+            bool course_offered = false;
+            for(int i = 0; i < offerings.size(); i++){
+                if(offerings[i].name == name){
+                    //implement the hash table and put it here so time
+                    //complexity is better instead of this for loop
+                    course_offered = true;
+                }
+            }
+        if(!course_offered){
+          return;
+        }
+        //continue for other checks
 
 }
